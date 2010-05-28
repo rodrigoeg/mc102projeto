92559b278b2a37de268062ba122733a0ff7919b5
rodrigoegoncalves <rodrigoegoncalves@c17bd793-d971-0999-8e81-da9e7e574f26>
2010-05-28 03:28:58 +0000

    comit

---
+++ projeto_mc102/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: User
+ *
+ * Created on 28 de Maio de 2010, 00:22
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
