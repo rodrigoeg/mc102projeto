f99f2d3798e5bbc8d6addec2d984baf70af3bba7
rodrigoegoncalves <rodrigoegoncalves@c17bd793-d971-0999-8e81-da9e7e574f26>
2010-06-05 04:19:35 +0000

    Adicionado o sistema de colisao. pacman já anda. criado o arquivo carrega_fases.c

---
+++ trunk/mc_102_pacman/carrega_fases.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro.h>
+#include "carrega_fases.h"
+
+//A variável abaixo guarda o local onde estão as fases em arquivo texto.
+const char *ArqFases[] = {"fases/fase1.txt", "fases/fase2.txt", "fases/fase3.txt"};
+
+/*
+ struct dirent **namelist;
+int n;
+
+n = scandir(".", &namelist, 0, alphasort);
+if (n < 0)
+perror("scandir");
+else {
+while(n--) {
+printf("%s\n", namelist[n]->d_name);
+free(namelist[n]);
+}
+free(namelist);
+}
+ */
+
+/*
+Com base nas variáveis acima temos o tamanho da tela 640x480, conforme abaixo:
+   (iTamTilesX * iTilesX) = SCREEN_W;
+   (iTamTilesY * iTilesY) = SCREEN_H;
+   (32 * 20) = 640;
+   (32 * 15) = 480;
+ */
+
+/*
+A função abaixo é a mais importante com relação ao arquivo de fases. Ela serve
+para carregar os valores que estão dentro do arquivo de fase para a matriz.
+Com base na matriz iremos montar o cenário de cada fase e posicionar suas
+respectivas texturas ou objetos, dependendo de como você modificar o código.
+ */
+void CarregaMatriz(int Mapa[QTDE_FASES][2][TILES_X][TILES_Y]) {
+    int x = 0;
+    int y = 0;
+    int iFase = 0;
+
+    char sLinha[ TILES_X ]; // Qtde de caracteres por linha
+
+    //O FOR abaixo é para preencher todas as fases.
+    for (iFase = 0; iFase < QTDE_FASES; iFase++) {
+        // Abre o arquivo de fases para leitura com base no iFase e o Array de fases
+        FILE *fp = fopen(ArqFases[iFase], "r");
+
+
+        //Preenche a matriz na horizontal e vertical
+        for (y = 0; y < TILES_Y; y++) {
+            /*
+            Atribui na variável sLinha a linha atual do arquivo texto.
+            Toda vez que essa rotina for chamada ele pula para a proxima linha e
+            joga o valor para sLinha, até finalizar as 15 linhas.
+             */
+
+            fgets(sLinha, TAM_TILES_X, fp);
+
+            /*
+               Já com o sLinha preenchido, agora, o FOR abaixo varre todas as
+               posições dessa linha e acrescenta na linha da matriz. Após terminar
+               de varrer o sLinha sai do for e espera próxima linha, se houver.
+             */
+            for (x = 0; x < TILES_X; x++) {
+                Mapa[iFase][FUNDO][x][y] = sLinha[x];
+            }
+        }
+
+        fclose(fp);
+    }
+}
+
+/*
+A função abaixo é responsável por declarar e definir as imagens que iremos
+utilizar no Array Textura. Essa imagens devem estar dentro da pasta imagens no
+nosso projeto,caso contrário o programa irá apresentar a mensagem de erro fatal.
+ */
+void CarregaTexturas() {
+
+    Pacman = load_bitmap("imagens/pac-man.bmp", NULL);
+
+    // Define o índice 0 da textura como sendo parede
+    Textura[0] = load_bitmap("tiles/parede.bmp", NULL);
+
+    // Define o índice 1 da textura como sendo grama
+    Textura[1] = load_bitmap("tiles/grama.bmp", NULL);
+
+    // Define o índice 2 da textura como sendo chão
+    Textura[2] = load_bitmap("tiles/chao.bmp", NULL);
+
+}
+
+/*
+Essa função é responsável por desenhar as texturas do cenário. O cenário a ser
+carregado é informado através de um argumento. Essa função usa como base a matriz
+carregada anteriormente para desenhar as texturas.
+ */
+void DesenhaCenario(BITMAP *Cenario, int iFase, int Mapa[QTDE_FASES][2][TILES_X][TILES_Y]) {
+    int x = 0;
+    int y = 0;
+
+    for (y = 0; y < TILES_Y; y++) {
+        for (x = 0; x < TILES_X; x++) {
+            if ((char) Mapa[iFase][FUNDO][x][y] == '#') // Parede
+                draw_sprite(Cenario, Textura[0], x * TAM_TILES_X, y * TAM_TILES_Y);
+            else
+                if ((char) Mapa[iFase][FUNDO][x][y] == '-') // Grama
+                draw_sprite(Cenario, Textura[1], x * TAM_TILES_X, y * TAM_TILES_Y);
+            else
+                if ((char) Mapa[iFase][FUNDO][x][y] == '.') // Chão
+                draw_sprite(Cenario, Textura[2], x * TAM_TILES_X, y * TAM_TILES_Y);
+            else
+                if ((char) Mapa[iFase][FUNDO][x][y] == 'P') // Pacman
+                draw_sprite(Cenario, Pacman, x * TAM_TILES_X, y * TAM_TILES_Y);
+        }
+    }
+}
--- trunk/mc_102_pacman/main.c
+++ trunk/mc_102_pacman/main.c
@@ -2,133 +2,51 @@
 #include <stdlib.h>
 #include <time.h>
 #include <allegro.h>
-
-//A variável abaixo guarda o local onde estão as fases em arquivo texto.
-const char *ArqFases[] = {"fases/fase1.txt", "fases/fase2.txt", "fases/fase3.txt"};
-
-const int iQtdeFases = 3; // Jogo com 3 fases
-const int iTamTilesX = 32; // Largura do Tile
-const int iTamTilesY = 32; // Altura do Tile
-const int iTilesX = 20; // Qtde de Tiles na horizontal - 32*20 = 640;
-const int iTilesY = 15; // Qtde de Tiles na vertical   - 32*15 = 480;
-
-
-int fps = 0;
-int fps_antigo = 0;
-int fps_speed = 0;
-
-// prototipo do contador de frames
-void frame_rate();
-
-// prototipo do contador de velocidade
-void incrementa_speed();
-
-void Teclado();
-void CarregaMatriz(int [iQtdeFases][iTilesX][iTilesY]);
-
-
-void CarregaTexturas();
-void DesenhaCenario(BITMAP *, int, int [iQtdeFases][iTilesX][iTilesY]);
-
-/*
-Com base nas variáveis acima temos o tamanho da tela 640x480, conforme abaixo:
-   (iTamTilesX * iTilesX) = SCREEN_W;
-   (iTamTilesY * iTilesY) = SCREEN_H;
-   (32 * 20) = 640;
-   (32 * 15) = 480;
- */
+#include "carrega_fases.h"
 
 int iFaseAtual = 0; // Guarda o index da fase atual.
 
-//Variável do tipo BITMAP responsável por guardar as texturas
-BITMAP *Textura[3] = {NULL, NULL, NULL};
-BITMAP *Pacman;
-
-/*
-A função abaixo é a mais importante com relação ao arquivo de fases. Ela serve
-para carregar os valores que estão dentro do arquivo de fase para a matriz.
-Com base na matriz iremos montar o cenário de cada fase e posicionar suas
-respectivas texturas ou objetos, dependendo de como você modificar o código.
- */
-void CarregaMatriz(int Mapa[iQtdeFases][iTilesX][iTilesY]) {
+void anda_pacman(int Mapa[QTDE_FASES][2][TILES_X][TILES_Y], int direcao) {
     int x = 0;
     int y = 0;
-    int iFase = 0;
+    int posicao_x, posicao_y;
 
-    char sLinha[ iTilesX ]; // Qtde de caracteres por linha
-
-    //O FOR abaixo é para preencher todas as fases.
-    for (iFase = 0; iFase < iQtdeFases; iFase++) {
-        // Abre o arquivo de fases para leitura com base no iFase e o Array de fases
-        FILE *fp = fopen(ArqFases[iFase], "r");
-
-
-        //Preenche a matriz na horizontal e vertical
-        for (y = 0; y < iTilesY; y++) {
-            /*
-            Atribui na variável sLinha a linha atual do arquivo texto.
-            Toda vez que essa rotina for chamada ele pula para a proxima linha e
-            joga o valor para sLinha, até finalizar as 15 linhas.
-             */
-
-            fgets(sLinha, iTamTilesX , fp);
-
-            /*
-               Já com o sLinha preenchido, agora, o FOR abaixo varre todas as
-               posições dessa linha e acrescenta na linha da matriz. Após terminar
-               de varrer o sLinha sai do for e espera próxima linha, se houver.
-             */
-            for (x = 0; x < iTilesX; x++) {
-                Mapa[iFase][x][y] = sLinha[x];
+    for (y = 0; y < TILES_Y; y++) {
+        for (x = 0; x < TILES_X; x++) {
+            if ((char) Mapa[iFaseAtual][FUNDO][x][y] == 'P') { // Pacman
+                posicao_x = x;
+                posicao_y = y;
+                x = TILES_X;
+                y = TILES_Y;
             }
         }
-
-        fclose(fp);
     }
-}
 
-/*
-A função abaixo é responsável por declarar e definir as imagens que iremos
-utilizar no Array Textura. Essa imagens devem estar dentro da pasta imagens no
-nosso projeto,caso contrário o programa irá apresentar a mensagem de erro fatal.
- */
-void CarregaTexturas() {
-    // Define o índice 0 da textura como sendo parede
-    Textura[0] = load_bitmap("tiles/parede.bmp", NULL);
-
-    // Define o índice 1 da textura como sendo grama
-    Textura[1] = load_bitmap("tiles/grama.bmp", NULL);
-
-    // Define o índice 2 da textura como sendo chão
-    Textura[2] = load_bitmap("tiles/chao.bmp", NULL);
-
-    Pacman = load_bitmap("imagens/pac-man.bmp", NULL);
-}
-
-/*
-Essa função é responsável por desenhar as texturas do cenário. O cenário a ser
-carregado é informado através de um argumento. Essa função usa como base a matriz
-carregada anteriormente para desenhar as texturas.
- */
-void DesenhaCenario(BITMAP *Cenario, int iFase, int Mapa[iQtdeFases][iTilesX][iTilesY]) {
-    int x = 0;
-    int y = 0;
-
-
-    for (y = 0; y < iTilesY; y++) {
-        for (x = 0; x < iTilesX; x++) {
-            if ((char) Mapa[iFase][x][y] == '#') // Parede
-                draw_sprite(Cenario, Textura[0], x * iTamTilesX, y * iTamTilesY);
-            else
-                if ((char) Mapa[iFase][x][y] == '-') // Grama
-                draw_sprite(Cenario, Textura[1], x * iTamTilesX, y * iTamTilesY);
-            else
-                if ((char) Mapa[iFase][x][y] == '.') // Chão
-                draw_sprite(Cenario, Textura[2], x * iTamTilesX, y * iTamTilesY);
-                else
-                    if((char) Mapa[iFase][x][y] == 'P') // Pacman
-                        draw_sprite(Cenario, Pacman, x * iTamTilesX, y * iTamTilesY);
-        }
+    switch (direcao) {
+        case DIR_UP:
+            if (Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y - 1] != '#') {
+                Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y] = Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y - 1];
+                Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y - 1] = 'P';
+            }
+            break;
+        case DIR_DOWN:
+            if (Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y + 1] != '#') {
+                Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y] = Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y + 1];
+                Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y + 1] = 'P';
+            }
+            break;
+        case DIR_LEFT:
+            if (Mapa[iFaseAtual][FUNDO][posicao_x - 1][posicao_y] != '#') {
+                Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y] = Mapa[iFaseAtual][FUNDO][posicao_x - 1][posicao_y];
+                Mapa[iFaseAtual][FUNDO][posicao_x - 1][posicao_y] = 'P';
+            }
+            break;
+        case DIR_RIGHT:
+            if (Mapa[iFaseAtual][FUNDO][posicao_x + 1][posicao_y] != '#') {
+                Mapa[iFaseAtual][FUNDO][posicao_x][posicao_y] = Mapa[iFaseAtual][FUNDO][posicao_x + 1][posicao_y];
+                Mapa[iFaseAtual][FUNDO][posicao_x + 1][posicao_y] = 'P';
+            }
+            break;
     }
 }
 
@@ -136,7 +54,7 @@ void DesenhaCenario(BITMAP *Cenario, int iFase, int Mapa[iQtdeFases][iTilesX][iT
 A função abaixo é responsável por controlar as entradas do teclado, qualquer
 tecla pressionada durante a execução do jogo.
  */
-void Teclado() {
+void Teclado(int Mapa[QTDE_FASES][2][TILES_X][TILES_Y]) {
     /*
     Quando declaramos uma variável como sendo static quer dizer que mesmo saindo
     da função ela não vai perder o valor dela. Dessa forma, utilizamos a variável
@@ -148,23 +66,68 @@ void Teclado() {
     if (Buffer_Teclado == 0) {
         // Muda o Cenário caso aperte a tecla espaço
         if (key[KEY_SPACE]) {
-            if (iFaseAtual < (iQtdeFases - 1))
+            if (iFaseAtual < (QTDE_FASES - 1))
                 iFaseAtual++;
             else
                 iFaseAtual = 0;
 
             Buffer_Teclado = 30;
         }
+
+        if (key[KEY_UP]) {
+            Buffer_Teclado = 15;
+            anda_pacman(Mapa, DIR_UP);
+            printf("up \n");
+        }
+        if (key[KEY_DOWN]) {
+            Buffer_Teclado = 15;
+            anda_pacman(Mapa, DIR_DOWN);
+            printf("down \n");
+        }
+        if (key[KEY_LEFT]) {
+            Buffer_Teclado = 15;
+            anda_pacman(Mapa, DIR_LEFT);
+            printf("left \n");
+        }
+        if (key[KEY_RIGHT]) {
+            Buffer_Teclado = 15;
+            anda_pacman(Mapa, DIR_RIGHT);
+            printf("right \n");
+        }
+
     } else Buffer_Teclado--;
 }
 
-void frame_rate() {
-    fps_antigo = fps;
-    fps = 0;
-}
+int inicia_allegro() {
 
-void incrementa_speed() {
-    fps_speed++;
+    // inicialize o sistema do Allegro
+    allegro_init();
+
+    // inicialize o controlador do teclado
+    install_keyboard();
+    install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, NULL);
+
+    install_timer();
+
+    //Habilita Acentuação
+    set_uformat(U_UTF8);
+
+    // definição da profundidade de cor
+    // http://pt.wikipedia.org/wiki/Profundidade_de_cor
+    set_color_depth(desktop_color_depth());
+
+    srand((unsigned) time(NULL));
+
+
+    // inicialize o modo gráfico com uma resolução de tela
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0)) {
+        allegro_message(allegro_error);
+        return FALSE;
+    }
+
+    set_window_title("Pacman");
+
+    return TRUE;
 }
 
 int main() {
@@ -173,19 +136,12 @@ int main() {
          Variáveis Mapa tridimensional sendo o primeiro ecrãn a Fase e os
         outros dois correspondem a quantidade de Tiles na horizontal e vertical.
      */
-    allegro_init();
-    install_keyboard();
-    set_color_depth(32);
-    set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
-    set_window_title("Fases em arquivos textos");
 
-    int Mapa[iQtdeFases][iTilesX][iTilesY];
-    install_timer();
-    install_int(frame_rate, 1000);
-    install_int_ex(incrementa_speed, BPS_TO_TIMER(60));
+    if (!inicia_allegro()) {
+        return EXIT_FAILURE;
+    }
 
-    //Habilita Acentuação
-    set_uformat(U_ASCII);
+    int Mapa[QTDE_FASES][2][TILES_X][TILES_Y];
 
     BITMAP *Buffer = NULL;
     Buffer = create_bitmap(SCREEN_W, SCREEN_H);
@@ -194,20 +150,15 @@ int main() {
     CarregaTexturas();
 
     while (!key[KEY_ESC]) {
-        while (fps_speed > 0) {
-            clear_bitmap(Buffer);
-            DesenhaCenario(Buffer, iFaseAtual, Mapa);
-            Teclado();
+        clear_bitmap(Buffer);
+        DesenhaCenario(Buffer, iFaseAtual, Mapa);
+        Teclado(Mapa);
 
-            textprintf_ex(Buffer, font, 8, 10, makecol(255, 255, 0), -1, "FPS: %d", fps_antigo);
-            textprintf_centre_ex(Buffer, font, SCREEN_W / 2, (SCREEN_H / 2) - 10, makecol(255, 255, 0),
-                    -1, "FASE: %d", iFaseAtual + 1);
-            textprintf_centre_ex(Buffer, font, SCREEN_W / 2, SCREEN_H / 2, makecol(255, 255, 0), -1,
-                    "Pressione espaço para mudar de fase.");
+        textprintf_centre_ex(Buffer, font, SCREEN_W / 2, (SCREEN_H / 2) - 10, makecol(255, 255, 0),
+                -1, "FASE: %d", iFaseAtual + 1);
+        textprintf_centre_ex(Buffer, font, SCREEN_W / 2, SCREEN_H / 2, makecol(255, 255, 0), -1,
+                "Pressione espaço para mudar de fase.");
 
-            fps_speed--;
-            fps++;
-        }
         blit(Buffer, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
         vsync();
     }
@@ -219,6 +170,6 @@ int main() {
     destroy_bitmap(Textura[2]);
     allegro_exit();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 END_OF_MAIN();
